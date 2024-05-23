bf93e25b4829b79fb5f326b5296a18e997080c45
Caeeuphrasio <128739177+Caeeuphrasio@users.noreply.github.com>
2024-05-23 15:39:42 -0300

    Modificações 3.0
    Co-authored-by: Luiz <luiz.felipesenac@hotmail.com>

--- src/src/pages/TelaPrincipal.js
+++ src/src/pages/TelaPrincipal.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList, } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // Importe o ícone necessário
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { List } from 'react-native-paper';
 
 import Body from '../components/Body';
@@ -10,38 +9,39 @@ import ImageLogo from '../components/ImageLogo';
 
 import { getTreinos } from '../services/WorkoutServices';
 import { useIsFocused } from '@react-navigation/native';
-import { useNavigation } from '@react-navigation/native'; // Importe a função useNavigation para acessar a navegação
+import { useNavigation } from '@react-navigation/native';
 
 const TelaPrincipal = () => { 
-  
-  const navigation = useNavigation('QuitarDivida'); // Obtenha a função de navegação
+  const navigation = useNavigation();
   const isFocused = useIsFocused();
   const [treinos, setTreinos] = useState([]);
 
   useEffect(() => {
-    getTreinos().then((dados) => {
-      setTreinos(dados);
-    });
+    if (isFocused) {
+      getTreinos().then((dados) => {
+        setTreinos(dados);
+      });
+    }
   }, [isFocused]);
 
   // Calcula o valor total do treino somando os valores de treino de cada item em DATA
-  const totalTreino = treinos.reduce((total, treino) => total + treino.valor, 0);
+  const totalTreino = treinos.reduce((total, treino) => total + (!treino.pago ? parseFloat(treino.valor) : 0), 0);
 
   // Agrupa os treinos pelo nome e soma seus valores
   const treinosAgrupados = treinos.reduce((accumulator, treino) => {
     if (accumulator[treino.nome]) {
-      accumulator[treino.nome].valorTotal += treino.valor;
+      accumulator[treino.nome].valorTotal += !treino.pago ? parseFloat(treino.valor) : 0;
     } else {
       accumulator[treino.nome] = {
         ...treino,
-        valorTotal: treino.valor
+        valorTotal: !treino.pago ? parseFloat(treino.valor) : 0
       };
     }
     return accumulator;
   }, {});
 
   // Converte o objeto de treinos agrupados em uma lista
-  const treinosUnicos = Object.values(treinosAgrupados);
+  const treinosUnicos = Object.values(treinosAgrupados).filter(treino => treino.valorTotal > 0); // Filtra clientes com valorTotal > 0
 
   const renderTreinos = ({ item }) => (
     <List.Item
@@ -51,7 +51,7 @@ const TelaPrincipal = () => {
           <Text>{'R$' + item.valorTotal.toFixed(2)}</Text>
         </View>
       )}
-      onPress={() => navigation.navigate('QuitarDivida', { treino: item })}
+      onPress={() => navigation.navigate('QuitarDivida', { clienteNome: item.nome })}
     />
   );
 
