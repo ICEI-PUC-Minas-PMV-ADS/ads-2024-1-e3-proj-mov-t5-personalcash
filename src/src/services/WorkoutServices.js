9aab15e040374e876eb91bc64a55f0acd94a3873
Caeeuphrasio <128739177+Caeeuphrasio@users.noreply.github.com>
2024-05-23 15:23:27 -0300

    DataBaseServices 3.0

--- src/src/services/WorkoutServices.js
+++ src/src/services/WorkoutServices.js
@@ -4,29 +4,25 @@ const DB_EXEC_WORKOUT = DataBaseWorkout.getConnection();
 
 export const getTreinos = async () => {
   let results = await DB_EXEC_WORKOUT(`select * from treinos`);
-  //console.log(results);
   return results.rows._array;
 }
 
 // Inserir
 export const insertTreinos = async (param) => {
-  let results = await DB_EXEC_WORKOUT(`insert into treinos( nome, data, inicio, duracao, valor, pago) values(?,?,?,?,?,?)`, 
-    [param.nome, param.data, param.inicio, param.duracao, param.valor, param.pago]);
-    //console.log(results);
+  let results = await DB_EXEC_WORKOUT(`insert into treinos (nome, data, inicio, duracao, valor, pago) values (?, ?, ?, ?, ?, ?)`, 
+    [param.nome, param.data, param.inicio, param.duracao, param.valor, param.pago ? 1 : 0]);
   return results.rowsAffected;
 }
 
 // Editar
 export const updateTreinos = async (param) => {
-  let results = await DB_EXEC_WORKOUT(`update treinos set nome=?, data=?, inicio=?, duracao=?, valor=?, pago=? where id=?`, 
-    [param.nome, param.data, param.inicio, param.duracao, param.valor, param.pago, param.id]);
-    //console.log(results);
+  let results = await DB_EXEC_WORKOUT(`update treinos set nome = ?, data = ?, inicio = ?, duracao = ?, valor = ?, pago = ? where id = ?`, 
+    [param.nome, param.data, param.inicio, param.duracao, param.valor, param.pago ? 1 : 0, param.id]);
   return results.rowsAffected;
 }
 
 // Deletar
 export const deleteTreinos = async (id) => {
-  let results = await DB_EXEC_WORKOUT(`delete from treinos where id=?`, [id]);
-    //console.log(results);
+  let results = await DB_EXEC_WORKOUT(`delete from treinos where id = ?`, [id]);
   return results.rowsAffected;
 }
